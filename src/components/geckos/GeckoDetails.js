b9a3120327abcdfd5d27b8f80b39854371653864
heidijane <heidi@heidisprouse.com>
2020-05-01 15:15:34 -0500

    added basic CSS for the gecko details page

--- src/components/geckos/GeckoDetails.js
+++ src/components/geckos/GeckoDetails.js
@@ -1,5 +1,22 @@
-import React from "react"
+import React, { useContext } from "react"
+import { GeckoContext } from "./GeckoProvider"
+import "./GeckoDetails.css"
 
 export default ( {geckoId} ) => {
-    return <>Gecko Details for {geckoId}</>
+
+    const { geckos } = useContext(GeckoContext)
+
+    const currentUserId = parseInt(sessionStorage.getItem("activeUser"))
+
+    const currentGecko = geckos.find(gecko => gecko.id === geckoId)
+
+    return (
+    <article className="geckoDetails">
+        <section className="geckoDetails__leftColumn">
+            left column
+        </section>
+        <section className="geckoDetails__rightColumn">
+            right column
+        </section>
+    </article>)
 }
--- src/components/geckos/GeckoList.js
+++ src/components/geckos/GeckoList.js
@@ -10,24 +10,31 @@ export default ({ setPageState, setGeckoDetailsId }) => {
     const currentUserId = parseInt(sessionStorage.getItem("activeUser"))
 
     const userGeckos = geckos.filter(gecko => gecko.userId === currentUserId)
-    return (
-        <section className="geckoList">
-            {userGeckos.map(gecko => {
-                return (
-                    <Card 
-                        key={"gecko_"+gecko.id}
-                        className="geckoCard"
-                        onClick={
-                            () => {
-                                setGeckoDetailsId(gecko.id)
-                                setPageState("geckoDetails")
-                            } 
-                            }>
-                        <CardImg top width="100%" src={require("../images/sample.gif")} />
-                        <CardTitle className="geckoCard__title">{gecko.name}</CardTitle>
-                    </Card>
-                )
-            })}
-        </section>
-        )
+
+    if (userGeckos.length > 0) {
+        return (
+            <section className="geckoList">
+                {userGeckos.map(gecko => {
+                    return (
+                        <Card 
+                            key={"gecko_"+gecko.id}
+                            className="geckoCard"
+                            onClick={
+                                () => {
+                                    setGeckoDetailsId(gecko.id)
+                                    setPageState("geckoDetails")
+                                } 
+                                }>
+                            <CardImg top width="100%" src={require("../images/sample.gif")} />
+                            <CardTitle className="geckoCard__title">{gecko.name}</CardTitle>
+                        </Card>
+                    )
+                })}
+            </section>
+            )
+    } else {
+        return (<section className="geckoList">You haven't added any geckos yet!</section>)
+    }
+
+    
 }
