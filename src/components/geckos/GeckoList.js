98aaa4e9011812f3afe5815e0bdd173e37559083
heidijane <heidi@heidisprouse.com>
2020-05-01 14:14:55 -0500

    user's geckos now show up as cards on the My Geckos page, currently using a placeholder image

--- src/components/geckos/GeckoList.js
+++ src/components/geckos/GeckoList.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react"
 import { GeckoContext } from "./GeckoProvider"
+import { Card, CardImg, CardTitle } from "reactstrap"
+import "./GeckoList.css"
 
 export default () => {
 
@@ -9,12 +11,15 @@ export default () => {
 
     const userGeckos = geckos.filter(gecko => gecko.userId === currentUserId)
     return (
-        <>
+        <section className="geckoList">
             {userGeckos.map(gecko => {
                 return (
-                    <p key={"gecko_"+gecko.id}>{gecko.name}</p>
+                    <Card key={"gecko_"+gecko.id} className="geckoCard">
+                        <CardImg top width="100%" src={require("../images/sample.gif")} />
+                        <CardTitle className="geckoCard__title">{gecko.name}</CardTitle>
+                    </Card>
                 )
             })}
-        </>
+        </section>
         )
 }
