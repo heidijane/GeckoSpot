f9de2bb6e3bce7d46cdf32bbcf214316dc9f9592
heidijane <heidi@heidisprouse.com>
2020-05-08 14:11:48 -0500

    Updated nav bar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,11 +3,11 @@ import "./NavBar.css"
 
 export default ({ setPageState, logout } ) => {
     return (
-            <div id="navigation" className="d-flex justify-content-between">
-                <div className="d-flex justify-content-start">
-                <h3>GeckoSpot</h3>
+            <div id="navigation" className="d-flex justify-content-between align-items-center flex-wrap">
+                <div className="d-flex justify-content-start align-items-center flex-wrap">
+                <div className="navLogo" onClick={() => { setPageState("myGeckos")}}>GeckoSpot</div>
                     <div onClick={() => { setPageState("myGeckos")}} className="navLink">My Geckos</div>
-                    <div onClick={() => { setPageState("marketplace")}} className="navLink">Marketplace</div>
+                    <div onClick={() => { setPageState("marketplace")}} className="navLink border-left-0">Marketplace</div>
                 </div>
                     <div onClick={logout} className="navLink">Logout</div>
             </div>
