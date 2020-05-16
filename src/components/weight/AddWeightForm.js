a9cc5038299a87aa3b8d83c73e885ba5b518fc14
heidijane <heidi@heidisprouse.com>
2020-05-16 15:06:41 -0500

    gecko weights can be logged and now appear in a line graph

---
+++ src/components/weight/AddWeightForm.js
@@ -0,0 +1,119 @@
+import React, { useContext, useRef } from "react"
+import { Form, FormGroup, Label, Input, InputGroup,InputGroupAddon, InputGroupText, Button } from "reactstrap"
+import { GeckoContext } from "../geckos/GeckoProvider"
+import { WeightContext } from "./WeightProvider"
+import DatePicker from "reactstrap-date-picker"
+
+export default ({ toggle, geckoId }) => {
+
+    const { addWeight } = useContext(WeightContext)
+
+    const { geckos } = useContext(GeckoContext)
+    const currentUserId = parseInt(sessionStorage.getItem("activeUser"))
+    let currentUserGeckos = geckos.filter(gecko => gecko.userId === currentUserId)
+
+    //get the current date for use as the default value
+    const currentDate = new Date();
+    let defaultDate = currentDate.toISOString()
+
+    const geckoWeightToBeLogged = useRef()
+    const geckoWeight = useRef()
+    const weighDateInput = useRef()
+
+    const convertToTimestamp = (string) => {
+        return (Date.parse(string)/1000)
+    }
+
+    const createWeight = () => {
+        const parsedGeckoId = parseInt(geckoWeightToBeLogged.current.value)
+        const parsedGeckoWeight = parseInt(geckoWeight.current.value)
+
+        let errorTrigger = false
+
+        if (parsedGeckoId === 0) {
+            errorTrigger = true
+            window.alert("Please choose a gecko to log a weight for.")
+        }
+
+        if (parsedGeckoWeight === 0) {
+            errorTrigger = true
+            window.alert("You did not input a weight.")
+        }
+
+        //if user doesn't fill out the date set it to the current day
+        let weighDateTimestamp = convertToTimestamp(currentDate)
+        if (weighDateInput.current.state.value !== "") {
+           weighDateTimestamp = convertToTimestamp(weighDateInput.current.state.value)
+        }
+
+        if (errorTrigger === false) {
+            addWeight({
+                geckoId: parsedGeckoId,
+                weight: parsedGeckoWeight,
+                weighDate: weighDateTimestamp,
+                deleted: false
+            }).then(toggle)
+        }
+
+    }
+
+    return (
+        <Form>
+            <FormGroup>
+                <Label for="sellForm__geckoToBeSold">Choose a gecko...</Label>
+                <Input
+                innerRef={geckoWeightToBeLogged}
+                type="select"
+                name="geckoToBeSold"
+                id="sellForm__geckoToBeSold"
+                defaultValue={geckoId}
+            >
+                <option key={"feeder_default"} value="0">Please select...</option>
+                {
+                    currentUserGeckos.map(gecko => {
+                        return <option key={"geckoDropdown_"+gecko.id} value={gecko.id}>{gecko.name}</option>
+                    })
+                }
+                </Input>
+            </FormGroup>
+            <FormGroup>
+                <Label for="weightForm_geckoWeight">Weight</Label>
+                <InputGroup>
+                    <Input 
+                        innerRef={geckoWeight}
+                        type="number"
+                        min="1"
+                        max="200"
+                        step="1"
+                        placeholder="weight in grams"
+                        name="geckoWeight"
+                        id="weightForm_geckoWeight"
+                    />
+                    <InputGroupAddon addonType="append">
+                        <InputGroupText>grams</InputGroupText>
+                    </InputGroupAddon>
+                </InputGroup>
+            </FormGroup>
+            <FormGroup>
+                <Label for="mealForm__mealDate">Meal Date</Label>
+                <DatePicker
+                    ref={weighDateInput}
+                    id="mealForm__mealDate"
+                    name="mealDate"
+                    clearButtonElement="Clear"
+                    value={defaultDate}
+                />
+            </FormGroup>
+            <FormGroup>
+                <Button
+                    type="submit"
+                    onClick={
+                        evt => {
+                            evt.preventDefault() // Prevent browser from submitting the form
+                            createWeight()
+                    }}
+                >Log Weight</Button>
+            </FormGroup>
+        </Form>
+    )
+}
--- src/components/weight/WeightLog.js
+++ src/components/weight/WeightLog.js
@@ -1,7 +1,9 @@
-import React, { useContext } from "react"
-import { Card, CardHeader, CardBody, Button, Table } from "reactstrap"
+import React, { useContext, useState } from "react"
+import { Card, CardHeader, CardBody, Button, Table, Modal, ModalHeader, ModalBody } from "reactstrap"
 import { WeightContext } from "./WeightProvider"
 import { timestampToDateString } from "../../utilities/timestampToString"
+import AddWeightForm from "./AddWeightForm"
+import {Line} from 'react-chartjs-2';
 
 export default ({ geckoId }) => {
 
@@ -10,12 +12,60 @@ export default ({ geckoId }) => {
 
     geckoWeights.sort((a, b) => (a.weighDate > b.weighDate) ? 1 : (a.weighDate === b.weighDate) ? ((a.id > b.id) ? 1 : -1) : -1 )
 
+    //states for add weight modal
+    const [addWeightModal, setAddWeightModal] = useState(false)
+    const addWeightModalToggle = () => setAddWeightModal(!addWeightModal)
+
+    //make an array of weights for the graph
+    let weightArray = []
+    let dateLabels = []
+    geckoWeights.forEach(weight => {
+        const date = new Date(weight.weighDate * 1000)
+        const formatter = new Intl.DateTimeFormat('default', { month: 'short', year: 'numeric' })
+        const month = formatter.format(date)
+
+        weightArray.push({
+            x: month,
+            y: weight.weight
+        })
+
+        dateLabels.push(month)
+    })
+
+    const arrayUnique = (arr) => {
+        return arr.filter(function(item, index){
+            return arr.indexOf(item) >= index
+        })
+    }
+
+    //create the data for the graph
+    const graphData = {
+        labels: arrayUnique(dateLabels),
+        datasets: [
+          {
+            label: 'Weight',
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: weightArray
+          }
+        ]
+      }
+
+    console.log(weightArray)
+
+    const weightGraph = (
+        <Line data={graphData} />
+    )
+
     const weightTable = (
         <Table className="mealLog table-sm table-responsive">
                     <thead>
                         <tr>
                             <th className="align-middle border-top-0 pr-3">Date</th>
-                            <th className="align-middle w-100 border-top-0">Meal</th>
+                            <th className="align-middle w-100 border-top-0">Weight</th>
                             <th className="text-nowrap border-top-0"></th>
                         </tr>
                     </thead>
@@ -44,20 +94,31 @@ export default ({ geckoId }) => {
     )
 
     return (
+        <>
         <Card className="mt-3">
             <CardHeader className="align-middle">
                 Weight Log
                 <Button 
                     color="primary"
                     className="btn-sm float-right"
-                    // onClick={addMealModalToggle}
+                    onClick={addWeightModalToggle}
                     >
                         Log Weight
                 </Button>
             </CardHeader>
             <CardBody>
-                {geckoWeights.length > 0 ? weightTable : "You have not logged your gecko's weight yet."}
+                {geckoWeights.length > 0 ? (<>{weightGraph} {weightTable}</>) : "You have not logged your gecko's weight yet."}
             </CardBody>
         </Card>
+
+        <Modal isOpen={addWeightModal} toggle={addWeightModalToggle}>
+        <ModalHeader toggle={addWeightModalToggle}>
+        Log Weight
+        </ModalHeader>
+        <ModalBody>
+            <AddWeightForm toggle={addWeightModalToggle} geckoId={geckoId} />
+        </ModalBody>
+        </Modal>
+        </>
     )
 }
